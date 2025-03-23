d787070802f0015702a16664cb2ffaa1fb729bbf
Milos Vasic <i@mvasic.ru>
2025-03-23 17:57:30 +0300

    Do unsubscribe safely.

--- src/app/machines/machines.component.ts
+++ src/app/machines/machines.component.ts
@@ -1,10 +1,10 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
-import { Observable, map } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { MachineStatus } from '../interfaces/machine.interface';
 import { MachineStatusComponent } from './machine-status/machine-status.component';
 import { MachinesService } from '../services/machines.service';
@@ -27,13 +27,29 @@ import { Machine } from '../interfaces/machine.interface';
   ],
 })
 export class MachinesComponent implements OnInit {
-  public MachineStatus = MachineStatus;
   public machines$!: Observable<Machine[]>;
+  private subscriptions: Subscription = new Subscription();
 
   constructor(private machinesService: MachinesService) {}
 
   ngOnInit(): void {
-    this.machinesService.getMachineStatusChanges$().subscribe();
+    
+    const webSocketSubscription = this.machinesService
+      .getMachineStatusChanges$()
+      .subscribe();
+
+    const machinesSubscription = this.machinesService
+      .getAllCachedMachines$()
+      .subscribe();
+
+    this.subscriptions.add(webSocketSubscription);
+    this.subscriptions.add(machinesSubscription);
+
     this.machines$ = this.machinesService.getAllCachedMachines$();
+  }
+
+  ngOnDestroy(): void {
+
+    this.subscriptions.unsubscribe();
   }
 }
