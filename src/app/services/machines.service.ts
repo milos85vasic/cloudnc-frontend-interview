bd9957a288583be4dd9dc73fcc022cd0289d2633
Milos Vasic <i@mvasic.ru>
2025-03-23 16:06:33 +0300

    Working on the Service imple.

--- src/app/services/machines.service.ts
+++ src/app/services/machines.service.ts
@@ -1,20 +1,66 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Socket } from 'ngx-socket-io';
-import { Observable, scan } from 'rxjs';
-import { MachineStatusFromWebSocket } from '../interfaces/machine.interface';
+import { Observable, scan, mergeMap, tap, BehaviorSubject } from 'rxjs';
+import { Machine, MachineStatusFromWebSocket } from '../interfaces/machine.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MachinesService {
+  
+  private allEvents$ = new BehaviorSubject<MachineStatusFromWebSocket[]>([]);
+  private machinesCache$ = new BehaviorSubject<{ [machineId: string]: Machine }>({});
 
-  constructor(private socket: Socket) { }
+  constructor(private socket: Socket, private httpClient: HttpClient) {}
 
   public getMachineStatusChanges$(): Observable<MachineStatusFromWebSocket[]> {
     return this.socket
-    .fromEvent<MachineStatusFromWebSocket, 'MACHINE_STATUS_CHANGES'>('MACHINE_STATUS_CHANGES')
-    .pipe(
-      scan((statuses, status) => [...statuses, status], [] as MachineStatusFromWebSocket[])
+      .fromEvent<MachineStatusFromWebSocket, 'MACHINE_STATUS_CHANGES'>('MACHINE_STATUS_CHANGES')
+      .pipe(
+        tap((event) => {
+        
+          // Update the list of all events:
+          const currentEvents = this.allEvents$.getValue();
+          this.allEvents$.next([...currentEvents, event]);
+
+          // Fetch machine details if it's a new machine:
+          const machineId = event.id;
+          if (!this.machinesCache$.getValue()[machineId]) {
+            this.fetchMachineDetails(machineId).subscribe((machine) => {
+              const currentCache = this.machinesCache$.getValue();
+              this.machinesCache$.next({ ...currentCache, [machineId]: machine });
+            });
+          }
+      }),
+      scan(
+        (statuses, status) => [...statuses, status], [] as MachineStatusFromWebSocket[]
+      )
+    );
+  }
+
+  private fetchMachineDetails(machineId: string): Observable<Machine> {
+    return this.httpClient.get<Machine>(`http://localhost:3000/machines/${machineId}`);
+  }
+
+  public getAllEvents$(): Observable<MachineStatusFromWebSocket[]> {
+    return this.allEvents$.asObservable();
+  }
+
+  public getMachineDetails$(machineId: string): Observable<Machine | undefined> {
+    return this.machinesCache$.pipe(
+      mergeMap((cache) => {
+        if (cache[machineId]) {
+          return [cache[machineId]];
+        } else {
+          return this.fetchMachineDetails(machineId).pipe(
+            tap((machine) => {
+              const currentCache = this.machinesCache$.getValue();
+              this.machinesCache$.next({ ...currentCache, [machineId]: machine });
+            })
+          );
+        }
+      })
     );
   }
 }
