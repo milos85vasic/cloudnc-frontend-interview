c0c6b6f1b114e245c4e2792b5e1a1c79bd9f20f9
Milos Vasic <i@mvasic.ru>
2025-03-23 15:55:38 +0300

    Machines service - Listen.

--- src/app/services/machines.service.ts
+++ src/app/services/machines.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Observable, scan } from 'rxjs';
+import { MachineStatusFromWebSocket } from '../interfaces/machine.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MachinesService {
 
-  constructor() { }
+  constructor(private socket: Socket) { }
+
+  public getMachineStatusChanges$(): Observable<MachineStatusFromWebSocket[]> {
+    return this.socket
+    .fromEvent<MachineStatusFromWebSocket, 'MACHINE_STATUS_CHANGES'>('MACHINE_STATUS_CHANGES')
+    .pipe(
+      scan((statuses, status) => [...statuses, status], [] as MachineStatusFromWebSocket[])
+    );
+  }
 }
