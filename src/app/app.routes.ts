5b8d379949544d13f639a0c767d890225a767858
Milos Vasic <i@mvasic.ru>
2025-03-23 15:22:43 +0300

    Small cleanup.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { MachinesComponent } from './machines/machines.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
 
 export const routes: Routes = [
-  // 'machines/:id' is not defined as a children of 'machines' on purpose
-  // please keep it that way
+  /*
+    'machines/:id' is not defined as a children of 'machines' on purpose
+    please keep it that way
+  */
   { path: 'machines/:id', component: MachineComponent },
   { path: 'machines', component: MachinesComponent },
   { path: 'sandbox', component: SandboxComponent },
